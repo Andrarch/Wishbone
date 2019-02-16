5f172a15900d09a99344b890882e512d6e846035
zeyang hu <hu000073@algonquinlive.com>
2019-02-16 18:50:28 +0000

    ZeyangBranch Feb 16,2019

---
+++ Wishbone/dao/authenticationDAO.php
@@ -0,0 +1,38 @@
+<?php
+require_once('abstractDAO.php');
+require_once('./models/authentication.php');
+
+    class AuthenticationDAO extends AbstractDAO {
+    
+        function __construct() {
+            try{
+                parent::__construct();
+            } catch(mysqli_sql_exception $e){
+                throw $e;
+            }
+        }
+        
+
+        public function addNewRegistrant($Registrant){
+            if($this->mysqli->connect_errno){
+                $query = 'INSERT INTO authentication
+                            (email, pass) VALUES(?,?)';
+                
+                $email = $Registrant->getRegistrantEmail();
+                $pass = $Registrant->getRegistrantPassword();
+                
+                $stmt = $this->mysqli->prepare($query);
+                $stmt->bind_param('ss', $email,$pass);
+                $stmt->execute();
+                if($stmt->error){
+                    return $stmt->error;
+                }else{
+                    return $Registrant->getRegistrantEmail().' '.$Registrant->getRegistrantPassword().' added successfully';
+                }
+            }else{
+                return 'Could not connect to Database';
+            }
+        }
+       
+    }
+?>
