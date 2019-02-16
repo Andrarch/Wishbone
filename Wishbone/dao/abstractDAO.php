2410f39f4d7afbc275114810b17180ace50fd77f
Minyi Yang <minyi.yang@hotmail.com>
2019-02-16 12:52:43 -0500

    profile_simple mvc

---
+++ Wishbone/dao/abstractDAO.php
@@ -0,0 +1,26 @@
+<?php
+
+mysqli_report(MYSQLI_REPORT_STRICT);
+
+class AbstractDAO{
+	protected $mysqli;
+    
+    protected static $DB_HOST= "localhost";
+	protected static $DB_USERNAME = "root";
+	protected static $DB_PASSWORD = "root";
+	protected static $DB_DATABASE = "wishbone";
+	
+	function __construct(){
+		try{
+			$this->mysqli = new mysqli(self::$DB_HOST,self::$DB_USERNAME, self::$DB_PASSWORD, self::$DB_DATABASE);
+		}catch(mysqli_sql_exceptioin $e){
+			throw $e;
+		}
+			
+	}
+	
+	public function getMysqli(){
+		return $this->mysqli;	
+	}
+}
+?>
