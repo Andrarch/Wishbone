bfa621df9aa215c0c32a617a1a1d2f60143c6e73
Ksenia <hgdryuk>
2019-03-11 14:55:46 +0000

    header changes in chat

--- Wishbone/chat.php
+++ Wishbone/chat.php
@@ -23,7 +23,11 @@ if( $receiver == null )
     exit("404 not found");
 }
 
-?><!DOCTYPE html>
+?>
+
+
+ 
+ <!DOCTYPE html>
 <html>
 <head>
 <title>Contact</title>
@@ -107,36 +111,15 @@ if( $receiver == null )
 </script>
 
 <body>
-	<div class="page-wrap">
 
-    <div class="page-wrap">
-        <!-- header -->
-        <header class="header">
-			<div class="container">
-				<div class="header__logo">
-					<a style="color: #f39c12; font-size: 25px; font-weight: 700;" href="index.html">WISHBONE</a>
-				</div>
-                
-                <nav class="consult-nav">
-                    <!-- consult-menu -->
-					<ul class="consult-menu">
-						<li><a href="index.html">Home</a></li>
-				        <li><a href="chat.php">My Messages</a></li>
-						<li><a href="mynetwork.php">My Network</a></li>
-						<li><a href="profile.php">My Profile</a>
-						<li><a href="userHome.php">My Page</a>
-						<li><a href="search.php">Search</a>
-						<li><a href="logOut.php">Log out</a>    
-					</ul>
-					<!-- consult-menu -->
-                    <div class="navbar-toggle">
-						<span></span><span></span><span></span>
-					</div>
-                </nav>
-				<!-- End / consult-nav -->
-            </div>
-        </header>
-    </div>
+ <?php 
+ 
+ include('header.php');
+ $header=new header();
+ $header->getHeader();
+ 
+ ?>
+
     
 
 		<!-- Content-->
