c7c3bc421401123da6a150e8e624d45aae9030f8
School <School@Andrew660>
2019-03-11 14:52:07 +0000

    Header.php

--- Wishbone/myNetwork.php
+++ Wishbone/myNetwork.php
@@ -1,5 +1,6 @@
 <?php include('dao/myNetworkDAO.php');
 include_once('model/myNetworkModel.php');
+include('header.php');
 ?>
 
 <!doctype html>
@@ -23,38 +24,13 @@ include_once('model/myNetworkModel.php');
   	<link rel="stylesheet" type="text/css" href="assets/css/myNetwork.css">
   </head>
   <body>
- <div class="page-wrap">
-        <!-- header -->
-        <header class="header">
-			<div class="container">
-				<div class="header__logo">
-					<a style="color: #f39c12; font-size: 25px; font-weight: 700;" href="index.html">WISHBONE</a>
-				</div>
-                
-                <nav class="consult-nav">
-                    <!-- consult-menu -->
-					<ul class="consult-menu">
-						<li><a href="index.html">Home</a></li>
-				        <li><a href="chat.php">My Messages</a></li>
-						<li><a href="mynetwork.php">My Network</a></li>
-						<li><a href="profile.php">My Profile</a>
-						<li><a href="userHome.php">My Page</a>
-						<li><a href="search.php">Search</a>
-						<li><a href="logOut.php">Log out</a>    
-					</ul>
-					<!-- consult-menu -->
-                    <div class="navbar-toggle">
-						<span></span><span></span><span></span>
-					</div>
-                </nav>
-				<!-- End / consult-nav -->
-            </div>
-    
-            
-        </header>
-        
-        
-    </div>
+ <?php 
+ $header=new header();
+ $header->getHeader();
+ 
+ ?>
+ 
+ 
    <div class="myNetwork" style="margin-top: 100px"> 
 	<?php 
 	session_start();
