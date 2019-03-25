67cac1573e40ca0aa47e5bc8afbe09e52879da9f
roxan <shap0015@algonquinlive.com>
2019-03-25 15:17:32 +0000

    Oksana 'Last' Edit

--- Wishbone/searchFeed.php
+++ Wishbone/searchFeed.php
@@ -1,28 +1,3 @@
- <?php
-	include('config.php');
-session_start();
-   
-$user_check = $_SESSION['useremail'];
-
-$infoQuery ="SELECT users.imagelocation, users.userid, users.firstname, users.lastname 
-		FROM users JOIN authentication
- 			ON authentication.authid = users.authid
- 			WHERE authentication.email = '$user_check'";
-
-$result = mysqli_query($connection, $infoQuery) or die(mysqli_error($connection));
-
-$row = mysqli_fetch_array($result, MYSQLI_ASSOC) or die(mysqli_error($connection));
-	
-$Login_user_imagelocation=$row['imagelocation'];	
-$login_user_firstname = $row['firstname'];
-$login_user_lastname = $row['lastname'];
-$login_user_userid = $row['userid'];
-	
-$_SESSION['userfirstname'] = $login_user_firstname;
-$_SESSION['userlastname']=$login_user_lastname;
- $_SESSION['userid']=$login_user_userid;
- 	mysqli_close($connection);
- ?>
 <html>
 <head>
 
@@ -47,7 +22,7 @@ $_SESSION['userlastname']=$login_user_lastname;
 
 </head>
 
-  <form action="/wishbone/insertPost.php" method="post"> 
+<form action="http://localhost:8728/Wishbone/insertPost.php" method="post"> 
 <div class="page-wrap">
 
 		<!-- header -->
@@ -67,7 +42,7 @@ $_SESSION['userlastname']=$login_user_lastname;
 						<li><a href="profile.php">My Profile</a>
 						<li><a href="userHome.php">My Page</a>
 						<li><a href="search.php">Search</a>
-						<li><a href="index.html">Log out</a>
+						<li><a href="logout.php">Log out</a>
 					</ul>
 					<!-- consult-menu -->
 
@@ -80,9 +55,9 @@ $_SESSION['userlastname']=$login_user_lastname;
 		</header>
     
     </div>
-
-
-<div class="mycontent">
+    
+   
+   <div class="mycontent">
 
 <div class="leftContent">
 	<div class="card">
@@ -90,7 +65,7 @@ $_SESSION['userlastname']=$login_user_lastname;
 	
 	<img src="wishbone pictures/oksana2.jpg" alt="oksana2" class="oksana2" style="width:100%">
      
-       <img src=<?php echo "\"".$Login_user_imagelocation."\"" ?> alt="oksana" class="oksana" style="width:70%">
+       <img src="http://chittagongit.com/images/default-user-icon/default-user-icon-8.jpg" alt="oksana" class="oksana" style="width:70%">
     </div>
 	 
 	
@@ -107,11 +82,9 @@ $_SESSION['userlastname']=$login_user_lastname;
 	</div> 
 	</div>
 	<div class="rightContent">
-	<form action ="/wishbone/insertPost.php" method="post">
+	<form action ="http://localhost:8728/Wishbone/insertPost.php" method="post" width=100px>
 		<div class="posts">
-			<textarea class="serverText" name= "dataPost">
-   Type your post here
-  </textarea>
+			<textarea class="serverText" placeholder="type your post" name= "dataPost" required></textarea>
   <div class="button">
   <button class ="createPost" type="submit">Create Post</button> 
   	</div>
@@ -129,22 +102,72 @@ $_SESSION['userlastname']=$login_user_lastname;
 	    <div class="centerFeed">
 
 	    	<?php
-	    		require_once('dao/user_home_dao.php');
-	    		$dao = new user_home_dao();
-				$conn = $dao->getMysqli();
-				$sql = "select u.firstname, u.lastname, f.feedtext, f.feeddate from users u, feeds f where f.userid = u.userid order by feeddate DESC ";
-				$result = $conn->query($sql);
+// 	    		require_once('dao/user_home_dao.php');
+// 	    		$dao = new user_home_dao();
+// 				$conn = $dao->getMysqli();
+// 				$sql = "select u.firstname, u.lastname, f.feedtext, f.feeddate from users u, feeds f where f.userid = u.userid order by feeddate DESC ";
+// 				$result = $conn->query($sql);
                 
-				if ($result->num_rows > 0) {
-				    while($row = $result->fetch_assoc()) {
-				        echo '<p><b>'.$row["feeddate"].'  '.$row["firstname"].'  '.$row["lastname"].'</b></p>';
-				        echo '<p>'.$row['feedtext'].'</p><br>' ;
-				    }
-				} else {
-				    echo "0 results";
+// 				if ($result->num_rows > 0) {
+// 				    while($row = $result->fetch_assoc()) {
+// 				        echo '<p><b>'.$row["feeddate"].'  '.$row["firstname"].'  '.$row["lastname"].'</b></p>';
+// 				        echo '<p>'.$row['feedtext'].'</p><br>' ;
+// 				    }
+// 				} else {
+// 				    echo "0 results";
+// 				}
+// 				$conn->close();
+				
+				
+				
+				$searchFeed=$_GET['input'];
+			
+				$link = mysqli_connect("localhost", "root", "", "wishbone");
+				
+				$arr = $keywords = preg_split("/[\s,]+/", $searchFeed);
+				if(sizeof($arr)==1){
+				    $select=mysqli_query($link, "select u.firstname, u.lastname, f.feedtext, f.feeddate from users u, feeds f where f.userid = u.userid and (f.feedtext like '%$searchFeed%' or u.firstname like '%$searchFeed%' or u.lastname like '%$searchFeed%') order by feeddate DESC");
+				}else if(sizeof($arr==2)){
+				    $select=mysqli_query($link, "select u.firstname, u.lastname, f.feedtext, f.feeddate from users u, feeds f where f.userid = u.userid and (f.feedtext like '%$searchFeed%' or u.firstname like '%$arr[0]%' or u.lastname like '%$arr[1]%') order by feeddate DESC");
 				}
-				$conn->close();
+				
+				
+			
+				while ($rows=mysqli_fetch_assoc($select)){
+				    
+// 				    echo '<p><b>'.$rows["feeddate"].'  '.$rows["firstname"].'  '.$rows["lastname"].'</b></p>';
+// 				    echo '<p>'.$rows['feedtext'].'</p><br>' ;
+                    
+				    echo '<p><b>'.$rows["feeddate"].'  '.$rows["firstname"].'  '.$rows["lastname"].'</b></p>';
+				     //echo '<p>'.$rows['feedtext'].'</p><br>' ;
+				    $haystack=$rows['feedtext'];
+				    $needle=$searchFeed;
+				    $pos=stripos($rows['feedtext'],$needle);
+				    
+				    $bodytag=str_ireplace($needle, "<b> $needle </b>", $rows['feedtext']);
+				    echo $bodytag;
+				    
+// 				    if ($pos===false){
+// 				        echo"No luck";
+// 				    }
+// 				    else{
+// 				        echo"You got it!\n";
+// 				        echo $pos;
+// 				        echo $rows['feedtext'][3];
+				        
+// 				        $text = substr($haystack, 0, strripos($haystack, $s1)) . $s2 . substr($haystack,  
+// 				        strripos($haystackt, $s1)+strlen($s1), strlen($haystack) - (strripos($haystack, $s1)+strlen($s1) ));
+                        echo "<br>";
+// 				        $final=substr($haystack, 0, 5);
+// 				        echo $final;
+//                         $bodytag=str_ireplace($needle, "<b> $needle </b>", $rows['feedtext']);
+// 				        echo $bodytag;
+				   // }
+				    
+ 
+				}
 
+				
 	    	?>
 	   
 	   
@@ -162,8 +185,6 @@ $_SESSION['userlastname']=$login_user_lastname;
 
 	</div>
 </div>
-
-
 
 </form>
 <!-- footer -->
@@ -224,3 +245,4 @@ $_SESSION['userlastname']=$login_user_lastname;
      
 </body>
 </html>
+
