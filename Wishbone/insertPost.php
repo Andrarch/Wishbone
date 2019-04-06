1cd0b1e6f158d43c6f4f150e897c46ee02ac1029
roxan <shap0015@algonquinlive.com>
2019-04-06 16:52:49 +0000

    hope final

--- Wishbone/insertPost.php
+++ Wishbone/insertPost.php
@@ -1,14 +1,15 @@
 <?php
+include 'searchFeed.php';
 $data=$_POST["dataPost"];
-$userID=3;
 require_once('dao/user_home_dao.php');
 $dao = new user_home_dao();
 $conn = $dao->getMysqli();
+
 //$sql = "select u.firstname, u.lastname, f.feedtext, f.feeddate from users u, feeds f where f.userid = u.userid order by feeddate DESC ";
-$sql ="insert into feeds values(3, $data,current_time())";
+$sql ="insert into feeds values($login_user_userid, $data,current_time())";
 $result = $conn->query($sql);
 $link = mysqli_connect("localhost", "root", "", "WishBone");
-$insert=mysqli_query($link, "insert into feeds (userid, feedtext, feeddate) values (3, '$data', current_time())");
+$insert=mysqli_query($link, "insert into feeds (userid, feedtext, feeddate) values ('$login_user_userid', '$data', current_time())");
 echo "<script>location.href='userHome.php'</script>";
 
 
