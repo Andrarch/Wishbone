ff5e180c0a3daf613be8f7d294a855191fbab79e
zeyang hu <hu000073@algonquinlive.com>
2019-04-07 15:30:18 +0000

    AuthenticationDAO updated (auth, user, artist, artprofile, artist_artform, artist_interest, 7 tables new records insert)

--- Wishbone/dao/authenticationDAO.php
+++ Wishbone/dao/authenticationDAO.php
@@ -24,8 +24,8 @@ require_once('./model/authentication.php');
                 $stmt = $this->mysqli->prepare($query1);
                 $stmt->bind_param('ss', $email,$pass);
                 $stmt->execute();
+                //$stmt->close();
                 
-                // string md5( string )
 				
 				$query2 = 'INSERT INTO users(authid, firstname, lastname)
 							VALUES(LAST_INSERT_ID(),?,?)';
@@ -34,12 +34,117 @@ require_once('./model/authentication.php');
 				$stmt2 = $this->mysqli->prepare($query2);
 				$stmt2->bind_param('ss', $firstname,$lastname);
 				$stmt2->execute();
+				//$stmt2->close();
 				
-                if($stmt->error || $stmt2->error){
-                    return $stmt->error;
-                }else{
-                    return $Registrant->getRegistrantFirstName().' '.$Registrant->getRegistrantLastName().' '.$Registrant->getRegistrantEmail().' added successfully';
-                }
+				$query3 = 'INSERT INTO artists(userid) SELECT userid FROM users WHERE firstname = ? AND lastname = ?';
+				$stmt3 = $this->mysqli->prepare($query3);
+				$stmt3->bind_param('ss', $firstname, $lastname);
+				$stmt3->execute();
+				//$stmt3->close();
+
+				$query4 = 'INSERT INTO artprofile(artistid) VALUES (LAST_INSERT_ID())';
+				$stmt4 = $this->mysqli->prepare($query4);
+				$stmt4->execute();
+				//$stmt4->close();
+				
+				$query5 = 'SELECT artists.artistid FROM artists INNER JOIN users ON users.userid = artists.userid WHERE users.firstname = ? AND users.lastname = ?';
+				
+				if($stmt5 = $this->mysqli->prepare($query5)){
+					$stmt5->bind_param('ss', $firstname, $lastname);
+					$stmt5->execute();
+					$stmt5->bind_result($artistId);
+					$stmt5->fetch();
+					$stmt5->close();
+					
+										
+					$query6 = 'INSERT INTO artist_artform (artformid, artistid) VALUES (1,?)';
+					
+					if(false === $this->mysqli->prepare($query6)){
+						die('prepare() failed: ' . htmlspecialchars($this->mysqli->error));
+					}else{
+						$stmt6 = $this->mysqli->prepare($query6);
+						$stmt6->bind_param('i', $artistId);
+						$stmt6->execute();
+						$stmt6->close();
+					}
+					
+					$query7 = 'INSERT INTO artist_interest (interestid, artistid) VALUES (1,?)';
+					if(false === $this->mysqli->prepare($query7)){
+						die('prepare() failed: ' . htmlspecialchars($this->mysqli->error));
+					}else{
+						$stmt7 = $this->mysqli->prepare($query7);
+						$stmt7->bind_param('i', $artistId);
+						$stmt7->execute();
+						$stmt7->close();
+					}
+					
+					
+				}else{
+					$error = $this->mysqli->errno . ' ' . $this->mysqli->error;
+					 echo $error;
+				}
+				
+
+				//$query4 = 'SELECT artists.artistid FROM artists INNER JOIN users ON users.userid = artists.userid WHERE users.firstname = ? AND users.lastname = ?';
+				// $stmt4 = $this->mysqli->prepare($query4);
+				// $stmt4->bind_param('ss', $firstname, $lastname);
+				// $stmt4->execute();
+				// $stmt4->bind_result($artistId);
+				// $stmt4->fetch();
+				
+				
+				
+				
+				
+				// if($stmt4 = $this->mysqli->prepare($query4)){
+					// $stmt4->bind_param('ss', $firstname, $lastname);
+					// $stmt4->execute();
+					// $stmt4->bind_result($artistId);
+					// $stmt4->fetch();
+				// }else{
+					// $error = $this->mysqli->errno . ' ' . $this->mysqli->error;
+					 // echo $error;
+				// }
+				
+				
+				// echo $artistId;
+				
+				
+				
+				
+				
+				
+				
+				// $query5 = 'INSERT INTO artprofile(artistid) VALUES (?)';
+				// $stmt5 = $this->mysqli->prepare($query5);
+				// $stmt5->bind_param('i', $artistId);
+				// $stmt5->execute();
+				
+				
+				// $query5 = 'INSERT INTO artist_artform(artistid) VALUES (?);';
+				// $stmt5 = $this->mysqli->prepare($query5);
+				// $stmt5->bind_param(i, $artist_id);
+				// $stmt5->execute();
+				
+				
+				if($stmt->error){
+					return $stmt->error;
+				}else if($stmt2->error){
+					return $stmt2->error;
+				}else if($stmt3->error){
+					return $stmt3->error;
+				}else if($stmt4->error){
+					return $stmt4->error;
+				}else if($stmt5->error){
+					return $stmt5->error;
+				}else if($stmt6->error){
+					return $stmt6->error;
+				}else if($stmt7->error)
+					return $stmt67->error;
+				else{
+					return $Registrant->getRegistrantFirstName().' '.$Registrant->getRegistrantLastName().' '.$Registrant->getRegistrantEmail().' added successfully';
+				}
+
             }else{
                 return 'Could not connect to Database';
             }
